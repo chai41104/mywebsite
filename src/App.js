d6d1a2fa4bb918bb37d59e356b483f9ff60ff66b
Thongchai <chai41104@hotmail.com>
2017-12-28 15:09:05 +0000

    start to edit something.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React again</h1>
         </header>
         <p className="App-intro">
           I start to edit <code>src/App.js</code>.
