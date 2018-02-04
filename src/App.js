ffcb1da42b0485742b000a873b6b9f0ef6145d8c
Thongchai <chai41104@hotmail.com>
2018-02-04 15:57:58 +0000

    Adding material UI

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from './node_modules/material-ui/styles/MuiThemeProvider';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React again</h1>
-        </header>
-        <p className="App-intro">
-          I start to edit <code>src/App.js</code>.
-
-        </p>
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React again</h1>
+          </header>
+          <p className="App-intro">
+            I start to edit <code>src/App.js</code>.
+          </p>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
