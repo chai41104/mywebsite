e02f4638565a0d33cacb3b7b7de6d252f6e8e8b7
Thongchai <chai41104@hotmail.com>
2018-03-24 15:14:20 +0000

    Adding details of about-me page

--- src/component/AboutMe.js
+++ src/component/AboutMe.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
-import '../App.css';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import FontIcon from 'material-ui/FontIcon';
 
 class AboutMe extends Component {
   constructor(props) {
@@ -8,15 +9,39 @@ class AboutMe extends Component {
   }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React again</h1>
-        </header>
-        <p className="App-intro">
-          I start to edit <code>src/App.js</code>.
-        </p>
-      </div>
+      <Card>
+        <CardHeader
+          title="Thongchai Wirojsakseree"
+          subtitle="Software Development Engineer at Amazon"
+          avatar="images/myPicture.jpg"
+        />   
+        <CardText>
+          I am a full-stack developer with a strong programming background. For many years, I have been developing strong programming skills, algorithms, data structures and machine learning techniques. I also implemented many projects in C/C++, Java, JavaScript, Python and other computer languages. With innovative ideas, these projects have disrupted the traditional solutions and reduced a significant cost such as a visited museum app. On my free time, I also attend many online courses and many programming competitions such as Hacker Rank and Hackathon. In Hackathon, I have an excellence chance to discuss and learn with many experts in many technology areas as same as sharing many innovative ideas to change human’s life. In the meantime, I developed various useful skills such as fast-learning, problem-solving and adaptive skill. As a result, I have received many winner prizes in many hackathons such as BlackRock's Hackathon 2015.  
+        </CardText>
+        <CardActions>
+          <FlatButton
+            href="mailto:chaitype@gmail.com"
+            target="_blank"
+            label="chaitype@gmail.com"
+            primary={true}
+            icon={<a class="mdi mdi-email"></a>}
+          />
+          <FlatButton
+            href="https://github.com/chai41104"
+            target="_blank"
+            label="Github Profile"
+            primary={true}
+            icon={<a class="mdi mdi-github-box"></a>}
+          />
+          <FlatButton
+            href="https://www.linkedin.com/in/thongchai-wirojsakseree/"
+            target="_blank"
+            label="LinkedIn Profile"
+            primary={true}
+            icon={<a class="mdi mdi-linkedin-box"></a>}
+          />
+        </CardActions>
+      </Card>
     );
   }
 }
