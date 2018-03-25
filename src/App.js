1539a7c3f4bf6686b7e1944a5691c3258718797a
Thongchai <chai41104@hotmail.com>
2018-03-25 16:35:50 +0100

    Adding react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import MuiThemeProvider from './node_modules/material-ui/styles/MuiThemeProvider';
 import {ABOUTME_PAGE, RESUME_PAGE, PORTFOLIO_PAGE, CONTACT_PAGE} from './constant/page';
 import AboutMe from './component/AboutMe';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   constructor(props) {
@@ -13,23 +14,105 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        {this.displayContent()}
+        <Router>
+          <div>
+            {this.header()}
+            {this.mainSidebar()}
+            {this.content()}
+            {this.controlSidebar()}
+            {this.controlSidebarBG()}
+          </div>
+        </Router>
       </MuiThemeProvider>
     );
   }
-
-  displayContent() {
-    switch(this.state.page) {
-      case RESUME_PAGE:
-        return (<AboutMe />)
-      case PORTFOLIO_PAGE:
-        return (<AboutMe />)
-      case CONTACT_PAGE:
-        return (<AboutMe />)
-      case ABOUTME_PAGE:
-      default:
-        return (<AboutMe />)
-    }
+  header() {
+    return (
+      <div>
+        <header class="main-header">
+          <a href="index2.html" class="logo">
+            <span class="logo-mini">TC</span>
+            <span class="logo-lg">Thongchai</span>
+          </a>
+          <nav class="navbar navbar-static-top">
+            <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </a>
+            <div class="navbar-custom-menu">
+              <ul class="nav navbar-nav">
+                <li>
+                  <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </header>
+      </div>
+    );
+  }
+  mainSidebar() {
+    return (
+      <aside class="main-sidebar">
+        <section class="sidebar">
+          <ul class="sidebar-menu" data-widget="tree">
+            <li class="active treeview">
+              <Link to="/">
+                <i class="fa fa-pie-chart"></i>
+                <span>About Me</span>
+              </Link>
+            </li>
+            <li class="active treeview">
+              <Link to="/resume">
+                <i class="fa fa-pie-chart"></i>
+                <span>Resume (CV)</span>
+              </Link>
+            </li>
+            <li class="active treeview">
+              <Link to="/portfolio">
+                <i class="fa fa-pie-chart"></i>
+                <span>Portfolio</span>
+              </Link>
+            </li>
+            <li class="active treeview">
+              <Link to="/contact">
+                <i class="fa fa-pie-chart"></i>
+                <span>Contact</span>
+              </Link>
+            </li>
+          </ul>
+        </section>
+      </aside>
+    );
+  }
+  content() {
+    return (
+      <div class="content-wrapper">
+        <section class="content">
+          <Route exact path="/" component={AboutMe} />
+          <Route path="/resume" component={AboutMe} />
+          <Route path="/portfolio" component={AboutMe} />
+          <Route path="/contact" component={AboutMe} />
+        </section>
+      </div>
+    );
+  }
+  controlSidebar() {
+    return (
+      <aside class="control-sidebar control-sidebar-dark">
+        <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
+        </ul>
+        <div class="tab-content">
+          <div class="tab-pane" id="control-sidebar-home-tab">
+          </div>
+        </div>
+      </aside>
+    );
+  }
+  controlSidebarBG() {
+    return (<div class="control-sidebar-bg"></div>);
   }
 }
 
